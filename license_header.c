5bc7d7d1eef2e0b3ebfd8f039e61aa3b3c5ba163
Hugo Coto Florez <hugocoto100305@gmail.com>
2025-09-20 19:39:09 +0200

    Adding license and readme

---
+++ license_header.c
@@ -0,0 +1,17 @@
+/* This file is part of Lu programming language
+ *
+ * Copyright (C) 2025  Hugo Coto Flórez
+ *
+ * Lu is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * Lu is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY of FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details. 
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Lu. If not, see <https://www.gnu.org/licenses/>
+ *
+ * For questions or support, contact: hugo.coto@member.fsf.org
+ */
